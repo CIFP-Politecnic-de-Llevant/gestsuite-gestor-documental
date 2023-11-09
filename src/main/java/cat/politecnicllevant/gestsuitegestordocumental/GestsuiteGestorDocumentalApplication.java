fcca0bb5bf660ba1239adc2c59e26db48f47f073
Joan Galmés Riera <joan.galmes@gmail.com>
2023-11-09 11:07:59 +0100

    Init

---
+++ src/main/java/cat/politecnicllevant/gestsuitegestordocumental/GestsuiteGestorDocumentalApplication.java
@@ -0,0 +1,13 @@
+package cat.politecnicllevant.gestsuitegestordocumental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestsuiteGestorDocumentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestsuiteGestorDocumentalApplication.class, args);
+	}
+
+}
