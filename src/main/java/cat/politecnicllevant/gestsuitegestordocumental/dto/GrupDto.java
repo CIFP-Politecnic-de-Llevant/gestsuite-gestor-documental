0728701a99d88dba1cf1ac1ddbee0067a5fbda74
Joan Galmés Riera <joan.galmes@gmail.com>
2023-11-29 13:06:29 +0100

    Seguiment FCT. Llista de cursos amb tutor FCT

---
+++ src/main/java/cat/politecnicllevant/gestsuitegestordocumental/dto/GrupDto.java
@@ -0,0 +1,15 @@
+package cat.politecnicllevant.gestsuitegestordocumental.dto;
+
+import lombok.Data;
+
+public @Data class GrupDto {
+    private Long idgrup;
+    private String gestibIdentificador;
+    private String gestibNom;
+    private String gestibCurs;
+    private String gestibTutor1;
+    private String gestibTutor2;
+    private String gestibTutor3;
+    private Boolean actiu;
+    private String gsuiteUnitatOrganitzativa;
+}
