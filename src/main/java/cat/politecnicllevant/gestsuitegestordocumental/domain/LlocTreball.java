a9d7f467337b15b5fe9f4af49499562f59617b39
MarianMartorell <mmartorell398@alumnes.politecnicllevant.cat>
2024-04-19 13:31:00 +0200

    Classes d'empresa i lloc de treball fetes a Spring.

---
+++ src/main/java/cat/politecnicllevant/gestsuitegestordocumental/domain/LlocTreball.java
@@ -0,0 +1,62 @@
+package cat.politecnicllevant.gestsuitegestordocumental.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "pll_lloc_treball")
+public @Data class LlocTreball {
+
+    @Id
+    @Column(name = "idLlocEmpresa")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idLlocTreball;
+
+    @Column(name = "nom",nullable = true, length = 255)
+    private String nom;
+
+    @Column(name = "adreca",nullable = true, length = 255)
+    private String adreca;
+
+    @Column(name = "codi_potal",nullable = true)
+    private Long codiPostal;
+
+    @Column(name = "telefon",nullable = true, length = 128)
+    private String telefon;
+
+    @Column(name = "poblacio",nullable = true, length = 128)
+    private String poblacio;
+
+    @Column(name = "activitat",nullable = true, length = 128)
+    private String activitat;
+
+    @Column(name = "nom_representant_legal",nullable = true, length = 255)
+    private String nomRepresentantLegal;
+
+    @Column(name = "dni_representant_legal",nullable = true, length = 128)
+    private String dniRepresentantLegal;
+
+    @Column(name = "nom_tutor_empresa",nullable = true, length = 255)
+    private String nomTutorEmpresa;
+
+    @Column(name = "dni_tutor_empresa",nullable = true, length = 128)
+    private String dniTutorEmpresa;
+
+    @Column(name = "municipi",nullable = true, length = 128)
+    private String municipi;
+
+    @Column(name = "carrec_tutor",nullable = true, length = 128)
+    private String carrecTutor;
+
+    @Column(name = "email",nullable = true, length = 255)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn( name = "idEmpresa")
+    private Empresa empresa;
+
+
+
+
+
+}
