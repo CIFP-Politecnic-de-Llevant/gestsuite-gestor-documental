c7f153add146d9864fdd478181798bd316580569
Joan Galmés <joan.galmes@gmail.com>
2024-05-31 22:51:36 +0200

    minor fixes

--- src/main/java/cat/politecnicllevant/gestsuitegestordocumental/domain/DadesFormulari.java
+++ src/main/java/cat/politecnicllevant/gestsuitegestordocumental/domain/DadesFormulari.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "DadesAlumnes")
+@Document(collection = "alumnes")
 public @Data class DadesFormulari {
 
     @Id
